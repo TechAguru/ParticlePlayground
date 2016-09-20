54ad97cc671694d3e2b9636c25d73fe42a282bff
John Auxillos <john@techaguru.com>
2016-09-20 09:54:13 +0800

    Adding example for setting cellular credentials
    for Smart and Globe (PH Telecoms)

---
+++ ElectronCellularCredentials/ElectronCellularCredentials.ino
@@ -0,0 +1,13 @@
+// For Smart Communications APN Settings
+STARTUP(cellular_credentials_set("internet", "", "", NULL));
+
+// For Globe Telecommunications APN Settings
+//STARTUP(cellular_credentials_set("internet.globe.com.ph", "", "", NULL));
+
+void setup() {
+  // Your setup code here.
+}
+
+void loop() {
+  // Your main loop program
+}
